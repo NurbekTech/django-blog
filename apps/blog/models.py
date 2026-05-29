e05c715e63686df98d13682b55967ac6d3aa08a0
nurbekomar <omarnurbek13@gmail.com>
2026-05-29 14:28:01 +0500

    feat(blog): creat post model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,31 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="published")
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="blog_posts",
+    )
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ["-published"]
+        indexes = [models.Index(fields=["-published"])]
+
+    def __str__(self) -> str:
+        return self.title
