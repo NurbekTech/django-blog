493523120279f0a9049adf73b0b0d042d45033ec
nurbekomar <omarnurbek13@gmail.com>
2026-05-29 14:42:00 +0500

    feat(blog): add admin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from apps.blog.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "published", "status"]
+    list_filter = ["status", "created", "published", "author"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    autocomplete_fields = ["author"]
+    date_hierarchy = "published"
+    ordering = ["status", "published"]
+    list_select_related = ["author"]
